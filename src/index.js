196a256551840c3b65ee173c52c8c31c97d4139c
Rino <82651074+Rino9514@users.noreply.github.com>
2026-04-21 18:20:53 +0200

    add img, starting js and css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log("OUI");
+import {loadHome} from "./home.js"
+import "./styles.css"
+
+const index = (function(){
+  const header = document.querySelector("header");
+  const nav = document.querySelector("nav");
+  const div_content = document.querySelector("div#content");
+
+  const button_menu = document.createElement("button");
+  button_menu.textContent = "Menu";
+  button_menu.dataset.page = "menu";
+  const button_home = document.createElement("button");
+  button_home.textContent = "Home";
+  button_home.dataset.page = "home";
+  const button_contact = document.createElement("button");
+  button_contact.textContent = "Contact";
+  button_contact.dataset.page = "contact";
+
+
+  nav.appendChild(button_menu);
+  nav.appendChild(button_home);
+  nav.appendChild(button_contact);
+
+})();
+
